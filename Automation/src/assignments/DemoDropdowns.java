27d949cb626ffd56aef4b79548cf774d3d3176ee
Siddeshmgit <155037290+Siddeshmgit@users.noreply.github.com>
2023-12-28 09:26:54 +0530

    Update DemoDropdowns.java

--- Automation/src/assignments/DemoDropdowns.java
+++ Automation/src/assignments/DemoDropdowns.java
@@ -13,6 +13,7 @@ public static void main(String[] args) throws InterruptedException {
 	WebDriver driver=new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	//Launching facebook
 	driver.get("https://www.facebook.com/");
 	driver.findElement(By.linkText("Create new account")).click();
 	WebElement year = driver.findElement(By.id("year"));
