90f665483728a9c87ab26a6ced277f311caac6cf
QSP <QSP@Dell-Laptop-2nd-Floor-room1>
2024-01-04 08:25:51 +0530

    Wait

--- Automation/src/synchronisation/UsageOfFluentWait.java
+++ Automation/src/synchronisation/UsageOfFluentWait.java
@@ -5,15 +5,17 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
-
 public class UsageOfFluentWait {
 public static void main(String[] args) throws InterruptedException {
 	WebDriver driver=new ChromeDriver();
 	driver.manage().window().maximize();
 //	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	FluentWait wait=new FluentWait<>(driver);
-	wait.withTimeout(Duration.ofSeconds(5)).pollingEvery(Duration.ofSeconds(1));
+//	wait.withTimeout(Duration.ofSeconds(5)).pollingEvery(Duration.ofSeconds(1));
+	wait.withTimeout(Duration.ofSeconds(5)).pollingEvery(Duration.ofSeconds(1))
+	.until(ExpectedConditions.urlContains("orangehrm"));
 	driver.get("https://opensource-demo.orangehrmlive.com");
 //	Thread.sleep(2000);
 	driver.findElement(By.name("username")).sendKeys("admin");
